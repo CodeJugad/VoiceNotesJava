da2f6fc5c59effdc4496bb60657a41157cab36a2
CodeJugad <y314yadav@gmail.com>
2023-07-10 17:12:20 +0530

    delete all added

--- app/src/main/java/com/vinnovations/voicenotesjava/MainActivity.java
+++ app/src/main/java/com/vinnovations/voicenotesjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.vinnovations.voicenotesjava;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        Log.e("create", "create");
         setContentView(R.layout.activity_main);
+
 
         ListView listView = findViewById(R.id.listview);
 
@@ -70,6 +75,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item1) {
+            // Handle menu item 1 click
+            showDialogBox();
+            return true;
+        }
+//        } else if (id == R.id.menu_item2) {
+//            // Handle menu item 2 click
+//            return true;
+//        }
+        // Add more if-else blocks for additional menu items
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Delete All Notes")
+                .setMessage("You will not be able to access notes in future!")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle OK button click
+                        db.deleteAll(); // this is not working
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Handle Cancel button click
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 
 
 }
